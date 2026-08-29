dbb184bc063ebe300da5847efd66b7a33d78f9a7
tssmith1996 <40233119+tssmith1996@users.noreply.github.com>
2026-08-29 23:44:33 +0000

    Scaffolded API endpoints for users, workplaces, and shifts

--- routes/shifts.routes.js
+++ routes/shifts.routes.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const router = express.Router();
+// const {  } = require('../controllers/shifts.controller');
+
+// Create / Post a shift for cover
+router.post('/', );
+
+// Get Shifts
+router.get('/', );
+
+// Get Shift by ID
+router.get('/:id', );
+
+// Update Shift by ID
+router.put('/:id', );
+
+// Employee Claims Shift
+router.post('/:id/claim', );
+
+// Manager Approves / Rejects Employee Shift Claim
+router.put('/:id/claim', );
+
+// Original Employee Withdraws Shift
+router.post('/:id/withdraw', );
 
 module.exports = router;
--- routes/workplaces.routes.js
+++ routes/workplaces.routes.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const router = express.Router();
+// const {  } = require('../controllers/workplaces.controller');
+
+// Create workplace 
+router.post('/', );
+
+// Get all workplaces 
+router.get('/', );
+
+// Get workplace details  
+router.get('/:id', );
+
+// Update workplace details 
+router.put('/:id', );
+
+// Employee joins using invite code
+router.post('/join', );
+
+// Regenerate invite code   
+router.post('/:id/invite-code', );
 
 module.exports = router;
